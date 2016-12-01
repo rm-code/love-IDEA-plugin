fee06369cc2c5e0f285675efb800615abf4c6f5f
Robert Machmer <robert.machmer@gmail.com>
2016-12-02 00:15:40 +0100

    Add documentation for LÖVE 0.10.2

---
+++ love_0102/filesystem.lua
@@ -0,0 +1,36 @@
+module('love.filesystem')
+
+function append() end
+function areSymlinksEnabled() end
+function createDirectory() end
+function exists() end
+function getAppdataDirectory() end
+function getDirectoryItems() end
+function getIdentity() end
+function getLastModified() end
+function getRealDirectory() end
+function getRequirePath() end
+function getSaveDirectory() end
+function getSize() end
+function getSource() end
+function getSourceBaseDirectory() end
+function getUserDirectory() end
+function getWorkingDirectory() end
+function init() end
+function isDirectory() end
+function isFile() end
+function isFused() end
+function isSymlink() end
+function lines() end
+function load() end
+function mount() end
+function newFile() end
+function newFileData() end
+function read() end
+function remove() end
+function setIdentity() end
+function setRequirePath() end
+function setSource() end
+function setSymlinksEnabled() end
+function unmount() end
+function write() end
---
+++ love_0102/window.lua
@@ -0,0 +1,29 @@
+module('love.window')
+
+function close() end
+function fromPixels() end
+function getDisplayName() end
+function getFullscreen() end
+function getFullscreenModes() end
+function getIcon() end
+function getMode() end
+function getPixelScale() end
+function getPosition() end
+function getTitle() end
+function hasFocus() end
+function hasMouseFocus() end
+function isDisplaySleepEnabled() end
+function isMaximized() end
+function isOpen() end
+function isVisible() end
+function maximize() end
+function minimize() end
+function requestAttention() end
+function setDisplaySleepEnabled() end
+function setFullscreen() end
+function setIcon() end
+function setMode() end
+function setPosition() end
+function setTitle() end
+function showMessageBox() end
+function toPixels() end
