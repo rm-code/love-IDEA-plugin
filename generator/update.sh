5041d1363879f555905b0cb6a8ea37893704c32b
Robert Machmer <robert.machmer@gmail.com>
2017-01-31 14:08:33 +0100

    Fix generator script

--- generator/update.sh
+++ generator/update.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 readonly LOVE_DIR=love_0102
 
-rm -rf api
 git clone --depth=1 https://github.com/rm-code/love-api api
 mkdir docs
 lua generator.lua
-mv docs/ ../$LOVE_DIR
+mv -v docs/* ../$LOVE_DIR/
+rmdir docs
 rm -rf api
