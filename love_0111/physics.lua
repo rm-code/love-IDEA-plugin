f388492f8536d8879bdaff48985c2fabdf723c43
Vinicius Rangel <vinicius.l.rangel@gmail.com>
2018-10-11 09:58:58 -0300

    Add documentation for LÖVE 0.11.1

---
+++ love_0111/physics.lua
@@ -0,0 +1,24 @@
+module('love.physics')
+
+function getDistance() end
+function getMeter() end
+function newBody() end
+function newChainShape() end
+function newCircleShape() end
+function newDistanceJoint() end
+function newEdgeShape() end
+function newFixture() end
+function newFrictionJoint() end
+function newGearJoint() end
+function newMotorJoint() end
+function newMouseJoint() end
+function newPolygonShape() end
+function newPrismaticJoint() end
+function newPulleyJoint() end
+function newRectangleShape() end
+function newRevoluteJoint() end
+function newRopeJoint() end
+function newWeldJoint() end
+function newWheelJoint() end
+function newWorld() end
+function setMeter() end
