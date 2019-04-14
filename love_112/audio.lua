0a6070e183a214eb800318ae9c7a5a7e054ad258
Aleksandr Charkov <aleksandr.charkov@riseup.net>
2019-04-14 22:03:30 +0300

    add love 11.2v

---
+++ love_112/audio.lua
@@ -0,0 +1,29 @@
+module('love.audio')
+
+function getActiveEffects() end
+function getActiveSourceCount() end
+function getDistanceModel() end
+function getDopplerScale() end
+function getEffect() end
+function getMaxSceneEffects() end
+function getMaxSourceEffects() end
+function getOrientation() end
+function getPosition() end
+function getRecordingDevices() end
+function getSourceCount() end
+function getVelocity() end
+function getVolume() end
+function isEffectsSupported() end
+function newSource() end
+function newQueueableSource() end
+function pause() end
+function play() end
+function setDistanceModel() end
+function setDopplerScale() end
+function setEffect() end
+function setMixWithSystem() end
+function setOrientation() end
+function setPosition() end
+function setVelocity() end
+function setVolume() end
+function stop() end
---
+++ love_112/data.lua
@@ -0,0 +1,7 @@
+module('love.data')
+
+function compress() end
+function decode() end
+function decompress() end
+function encode() end
+function hash() end
